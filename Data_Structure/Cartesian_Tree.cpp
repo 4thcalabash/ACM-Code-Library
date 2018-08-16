e28336449cf3afc2a82b010d07d9098536dc97f2
161250136 <161250136@smail.nju.edu.cn>
2018-08-16 22:21:12 +0800

    add BerlekampMassey

---
+++ Data_Structure/Cartesian_Tree.cpp
@@ -0,0 +1,100 @@
+//
+// Created by calabash_boy on 18-7-24.
+//他的名字是笛卡尔树。
+//
+
+#include<bits/stdc++.h>
+using namespace std;
+#define OPENSTACK
+
+const int maxn = 1e6+100;
+const int mod = 1e9+7;
+typedef long long LL;
+int stk[maxn],top;
+int l[maxn],r[maxn],rt;
+int n;
+pair<int,int> a[maxn];
+LL inv[maxn];
+LL fac[maxn];
+LL inv_fac[maxn];
+int sz[maxn];
+bool vis[maxn];
+/* l 左儿子 r 右儿子 rt根*/
+void build(){
+    top=0;
+    for (int i=1;i<=n;i++) l[i]=r[i]=vis[i] =0;
+    for (int i=1;i<=n;i++){
+        int k = top;
+        while (k&&a[i]<a[stk[k-1]])k--;
+        if (k) r[stk[k-1]] = i;
+        if (k<top) l[i] = stk[k];
+        stk[k++] =i;
+        top = k;
+    }
+    for (int i=1;i<=n;i++) vis[l[i]] = vis[r[i]] =1;
+    for (int i=1;i<=n;i++){
+        if (!vis[i]){
+            rt = i;
+            break;
+        }
+    }
+}
+LL power(LL x,LL y){
+    LL res =1;
+    while (y){
+        if (y&1)res = res*x%mod;
+        y>>=1;
+        x = x*x%mod;
+    }
+    return res;
+}
+inline LL C(int n,int m){
+    return fac[n]*inv_fac[m]%mod*inv_fac[n-m]%mod;
+}
+int dfs(int u){
+    sz[u]=1;
+    int ans =1;
+    if (l[u])ans=1LL*ans*dfs(l[u])%mod;
+    if (r[u])ans = 1LL*ans*dfs(r[u])%mod;
+    sz[u]+=sz[l[u]]+sz[r[u]];
+    return 1LL*ans*C(sz[u]-1,sz[l[u]])%mod;
+}
+void Main(){
+    inv[1]=fac[1]=fac[0]=1;
+    for (int i=2;i<maxn;i++)fac[i] = fac[i-1]*i%mod,inv[i] = inv[mod%i]*(mod-mod/i)%mod;
+    inv_fac[maxn-1] = power(fac[maxn-1],mod-2);
+    for (int i=maxn-2;i>=0;i--){
+        inv_fac[i] = inv_fac[i+1]*(i+1)%mod;
+    }
+    int T;
+    scanf("%d",&T);
+    while (T--) {
+        scanf("%d", &n);
+        for (int i = 1; i <= n; i++) {
+            int x;
+            scanf("%d", &x);
+            a[i] = {-x, i};
+        }
+        build();
+        printf("%d\n", inv[2] * n % mod * power(fac[n], mod - 2) % mod * dfs(rt) % mod);
+    }
+}
+int main(){
+#ifdef OPENSTACK
+    int size = 70 << 20; // 256MB
+    char *p = (char*)malloc(size) + size;
+#if (defined _WIN64) or (defined __unix)
+    __asm__("movq %0, %%rsp\n" :: "r"(p));
+#else
+    __asm__("movl %0, %%esp\n" :: "r"(p));
+#endif
+#endif
+
+    Main();
+#ifdef OPENSTACK
+    exit(0);
+#else
+    return 0;
+#endif
+
+}
---
+++ Math/BerlekampMassey.cpp
@@ -0,0 +1,95 @@
+//
+// Created by calabash_boy on 18-8-16.
+//
+#include<bits/stdc++.h>
+#define FOR(i,l,r) for (int i = (l);i<(r);i++)
+#define FORD(i,r,l) for (int i= (r);i>(l);i--)
+using namespace std;
+typedef long long LL;
+typedef vector<LL> V;
+const int MOD = 1e9+7;
+
+// k 为 m 最高次数 且　a[m] == 1
+namespace BerlekampMassey {
+    inline void up(LL& a, LL b) { (a += b) %= MOD; }
+
+    V mul(const V& a, const V& b, const V& m, int k) {
+        V r; r.resize(2 * k - 1);
+        FOR (i, 0, k)
+            FOR (j, 0, k)
+                up(r[i + j], a[i] * b[j]);
+        FORD (i, k - 2, - 1) {
+            FOR (j, 0, k)
+                up(r[i + j], r[i + k] * m[j]);
+            r.pop_back();
+        }
+        return r;
+    }
+    LL pow_mod (LL x,LL y){
+        LL ret =1;
+        for (;y;y>>=1){if (y&1) ret = ret*x%MOD;x = x * x %MOD;}
+        return ret;
+    }
+    LL get_inv(LL x,LL MOD){
+        return pow_mod(x,MOD-2);
+    }
+    V pow(LL n, const V& m) {
+        int k = (int)m.size() - 1; assert(m[k] == -1 || m[k] == MOD - 1);
+        V r(k), x(k); r[0] = x[1] = 1;
+        for (; n; n >>= 1, x = mul(x, x, m, k))
+            if (n & 1) r = mul(x, r, m, k);
+        return r;
+    }
+    LL go(const V& a, const V& x, LL n) {
+        // a: (-1, a1, a2, ..., ak).reverse
+        // x: x1, x2, ..., xk
+        // x[n] = sum[a[i]*x[n-i],{i,1,k}]
+        int k = (int)a.size() - 1;
+        if (n <= k) return x[n - 1];
+        V r = pow(n - 1, a);
+        LL ans = 0;
+        FOR (i, 0, k)
+            up(ans, r[i] * x[i]);
+        return ans;
+    }
+
+    V BM(const V& x) {
+        V a = {-1}, b = {233};
+        FOR (i, 1, x.size()) {
+            b.push_back(0);
+            LL d = 0, la = a.size(), lb = b.size();
+            FOR (j, 0, la) up(d, a[j] * x[i - la  + 1 + j]);
+            if (d == 0) continue;
+            V t; for (auto& v: b) t.push_back(d * v % MOD);
+            FOR (j, 0, a.size()) up(t[lb - 1 - j], a[la - 1 - j]);
+            if (lb > la) {
+                b = a;
+                LL inv = -get_inv(d, MOD);
+                for (auto& v: b) v = v * inv % MOD;
+            }
+            a.swap(t);
+        }
+        for (auto& v: a) up(v, MOD);
+        return a;
+    }
+    void sample();
+}
+void BerlekampMassey::sample(){
+    V x(6);
+    x[0] = 1;
+    x[1] = 2;
+    x[2] = 21;
+    x[3] = 212;
+    x[4] = 2141;
+    x[5] = 21622;
+    V a = BerlekampMassey::BM(x);
+    cout<<"a[n] = ";
+    for (int i = 0;i<a.size()-2;i++){
+        cout<<a[i]<<"*a[n-"<<a.size()-1-i<<"] + ";
+    }
+    cout<<a[a.size()-2]<<"*a[n-1]"<<endl;
+}
+int main(){
+    BerlekampMassey::sample();
+    return 0;
+}
