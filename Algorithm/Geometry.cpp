33c2902c6570856830cf32f81184ced64fbce8e7
calabash_boy <751188973@qq.com>
2019-05-13 22:00:19 +0800

    add Minkowski Sum

--- Algorithm/Geometry.cpp
+++ Algorithm/Geometry.cpp
@@ -19,6 +19,7 @@ struct point{
     type sqrLen()const{return this->dot(*this);}
     type sqrDis(const point &p)const {return (p - *this).sqrLen();}
 };
+typedef point<long long> pt;
 namespace Geometry{
     const double PI = acos(-1.0);
     //res[0]: left most and bottom most
@@ -55,5 +56,51 @@ namespace Geometry{
         }
         return res;
     }
-
+    //calc the Minkowski Sum of two Convex Hull
+    vector<pt> Minkowski(const vector<pt> &ch1,const vector<pt> &ch2){
+        assert(ch1.size() >= 3);
+        assert(ch2.size() >= 3);
+        stack<pt> vec1;
+        stack<pt> vec2;
+        for (int i = ch1.size() - 1;i >=0;i--){
+            vec1.push(ch1[(i+1)%ch1.size()] - ch1[i]);
+        }
+        for (int i = ch2.size() - 1;i >= 0;i--){
+            vec2.push(ch2[(i+1)%ch2.size()] - ch2[i]);
+        }
+        vector<pt> res(0);
+        res.push_back(ch1.front() + ch2.front());
+        while (!vec1.empty() && !vec2.empty()){
+            auto v1 = vec1.top();
+            auto v2 = vec2.top();
+            long long cro = v1.cross(v2);
+            if (cro > 0){
+                res.push_back(res.back() + v1);
+                vec1.pop();
+            }else{
+                res.push_back(res.back() + v2);
+                vec2.pop();
+            }
+        }
+        while (!vec1.empty())res.push_back(res.back() + vec1.top()),vec1.pop();
+        while (!vec2.empty())res.push_back(res.back() + vec2.top()),vec2.pop();
+        return Convex_Hull(res);
+    }
+    //logn
+    //wether point in or on convex hull
+    bool within(pt p,const vector<pt> &ch){
+        assert(ch.size() >= 3);
+        auto base = ch.front();
+        if (base.cross(p,ch[1]) > 0 || base.cross(p,ch.back()) < 0)return false;
+        if (base.cross(p,ch[1]) == 0 && (p - base).sqrLen() <= (ch[1] - base).sqrLen())return true;
+        auto cmp = [&](const pt x,const pt y){
+            long long cro = base.cross(x,y);
+            return cro>0;
+        };
+        int i = lower_bound(ch.begin(),ch.end(),p,cmp) - ch.begin() - 1;
+        int j = i+1;
+        assert(j < ch.size());
+        return ch[i].cross(ch[j],p) >= 0;
+    }
 };
+
